60e8584b0e9f3eb6d5efae36de6fa89dfee949de
vsozansky <vsozansky@gmail.com>
2022-05-08 22:51:05 +0300

    Init commit

---
+++ src/main/java/cat/petproject/bionicproject/BionicProjectApplication.java
@@ -0,0 +1,13 @@
+package cat.petproject.bionicproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BionicProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BionicProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/petproject/bionicproject/BionicProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.petproject.bionicproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BionicProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
