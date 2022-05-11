eed786a0acf452393bda6ee13c3e02e832d90d90
vsozansky <vsozansky@gmail.com>
2022-05-11 21:48:06 +0300

    создал dto tickerPrice для получения текущей цены

---
+++ src/main/java/cat/petproject/bionicproject/dto/tickerPrice.java
@@ -0,0 +1,38 @@
+package cat.petproject.bionicproject.dto;
+
+import java.math.BigDecimal;
+
+public class tickerPrice {
+    private String symbol;
+    private BigDecimal price;
+
+    public tickerPrice(String symbol, BigDecimal price) {
+        this.symbol = symbol;
+        this.price = price;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "tickerPrice{" +
+                "symbol='" + symbol + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+}
