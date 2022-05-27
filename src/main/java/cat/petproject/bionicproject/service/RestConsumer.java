30e39ff910420eb72334cda413264a0a5e90b843
vsozansky <vsozansky@gmail.com>
2022-05-27 17:00:01 +0300

    доработал candlestick

--- src/main/java/cat/petproject/bionicproject/service/RestConsumer.java
+++ src/main/java/cat/petproject/bionicproject/service/RestConsumer.java
@@ -6,7 +6,10 @@ import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 @RestController
 public class RestConsumer {
@@ -32,26 +35,46 @@ public class RestConsumer {
         String resourceUrl = "https://api.binance.com/api/v3/ticker/price?symbols=[\"BTCUSDT\",\"ETHUSDT\",\"XRPUSDT\"]";
         HttpHeaders httpHeaders = getHttpHeaders();
         HttpEntity<String> entity = new HttpEntity<>(httpHeaders);
-        var result = restTemplate.exchange(resourceUrl, HttpMethod.GET, entity, TickerPrice[].class);
-        var bodyResult = result.getBody() ;
-        return bodyResult;
+        return restTemplate.exchange(resourceUrl, HttpMethod.GET, entity, TickerPrice[].class).getBody() ;
     }
 
     @GetMapping(value = "/candlestick")
-    public CandlestickData[] getCandlestickDataList() {
+    //public CandlestickData[] getCandlestickDataList() {
+    public List<CandlestickData> getCandlestickDataList() {
         //https://api.binance.com/api/v3/klines?symbol=ETHBTC&interval=1d&limit=1
         //[[1653004800000,"0.06661000","0.06760000","0.06648400","0.06749100","68734.52060000",1653091199999,"4609.71059174",83268,"34649.27850000","2323.90049805","0"]]
-        String resourceUrl = "https://api.binance.com/api/v3/klines?symbol=ETHBTC&interval=1d&limit=1";
+
+        String[] symbols = {"ETHBTC", "ETHUSDT", "XRPUSDT"};
+        String resourceUrl = "https://api.binance.com/api/v3/klines";
+        List<CandlestickData> candlestickDatas = new ArrayList<>();
+
         HttpHeaders httpHeaders = getHttpHeaders();
         HttpEntity<String> entity = new HttpEntity<>(httpHeaders);
 
-        //TODO переделать. сделать чтобы возвращал массив объектов
-        var resultString = restTemplate.exchange(resourceUrl, HttpMethod.GET, entity, String.class);
-        System.out.println(resultString.getBody());
-        return null;
-        /*
-        return restTemplate.exchange(resourceUrl, HttpMethod.GET, entity, CandlestickData[].class).getBody();
-        */
+        String getParameters;
+        List<List> resultList;
+        List result;
+        for (String symbol : symbols) {
+            getParameters = "?symbol=" + symbol + "&interval=1d&limit=1";
+            resultList = restTemplate.exchange(resourceUrl + getParameters, HttpMethod.GET, entity, ArrayList.class).getBody();
+            if (resultList != null) {
+                for (int i = 0; i < resultList.size(); i++) {
+                    result = (List) resultList.get(i);
+                    candlestickDatas.add(new CandlestickData(
+                            symbol
+                            , (Long) result.get(0) //Long openTime
+                            , (Long) result.get(6) //Long closeTime
+                            , new BigDecimal((String) result.get(1)) //BigDecimal open
+                            , new BigDecimal((String) result.get(2)) //BigDecimal high
+                            , new BigDecimal((String) result.get(3)) //BigDecimal low
+                            , new BigDecimal((String) result.get(4)) //BigDecimal close
+                            , new BigDecimal((String) result.get(5)) //BigDecimal volume
+                    ));
+                }
+            }
+        }
+        //return (CandlestickData[]) candlestickDatas.toArray();
+        return candlestickDatas;
     }
 
     /*
